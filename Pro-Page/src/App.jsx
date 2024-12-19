47f6cdcfd8b8eb8335a28d0659010b9fab32262e
Abhijeet <abhijeet1992tiwari@gmail.com>
2024-12-19 08:28:14 -0800

    Adding Route Pages

--- Pro-Page/src/App.jsx
+++ Pro-Page/src/App.jsx
@@ -1,6 +1,33 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import PageLayout from "./CommomCmpts/PageLayout"
+import ErrorPage from "./CommomCmpts/ErrorPage"
+import Home from './Routes/Home/Home'
+import EduAndSkills from "./Routes/EducationAndSkils/EduAndSkills"
+import Exp from "./Routes/ProfessionExperience/Exp"
+
+
 function App() {
+  const routes = createBrowserRouter([
+    {
+      element: <PageLayout/>,
+      errorElement: <ErrorPage/>,
+      children : [
+        {
+          path:"/",
+          element: <Home/>
+        },{
+          path: "/education-skills",
+          element: <EduAndSkills/>
+        },{
+          path: "/professional-exp",
+          element: <Exp/>
+        }
+      ]
+    }
+  ])
   return (
     <>
+      <RouterProvider router={routes}/>
     </>
   )
 }
---
+++ Pro-Page/src/Routes/EducationAndSkils/EduAndSkills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function EduAndSkills() {
+  return (
+    <div>
+      <h1>Education & Skills</h1>
+    </div>
+  )
+}
+
+export default EduAndSkills
